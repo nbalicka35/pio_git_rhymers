7c6bf602d6244b3bb23aba36d89ce3ac4d069cc7
nbalicka35 <245763@edu.p.lodz.pl>
2024-06-02 12:22:39 +0200

    14. Zmiana klasy Node na prywatna

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,39 +2,64 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
-    Node last;
-    int i;
-
+    private static final int Return_if_stack_is_empty = -1;
+    private Node last;
+    private int i;
     public void push(int i) {
         if (last == null)
             last = new Node(i);
         else {
-            last.next = new Node(i);
-            last.next.prev = last;
-            last = last.next;
+            last.setNext(new Node(i));
+            last.getNext().setPrev(last);
+            last = last.getNext();
         }
     }
-
     public boolean isEmpty() {
         return last == null;
     }
-
     public boolean isFull() {
         return false;
     }
-
     public int top() {
         if (isEmpty())
-            return -1;
-        return last.value;
+            return Return_if_stack_is_empty;
+        return last.getValue();
     }
-
     public int pop() {
         if (isEmpty())
-            return -1;
-        int ret = last.value;
-        last = last.prev;
+            return Return_if_stack_is_empty;
+        int ret = last.getValue();
+        last = last.getPrev();
         return ret;
     }
+    private class Node {
 
+        private final int value;
+        private Node prev;
+        private Node next;
+
+        public Node(int i) {
+            value = i;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
 }
